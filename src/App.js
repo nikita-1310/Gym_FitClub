f6db6125169d0c83b0eccb9daa83f3ed9c1b7ab2
nikita <nikitaagarwal8755@gmail.com>
2023-04-14 22:23:32 +0530

    Updtaed

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+// import Footer from './Components/Footer/Footer';
+// import Hero from './Components/Hero/Hero';
+// import Join from './Components/Join/Join';
+// import Plan from './Components/Plans/Plan';
+// import Programs from './Components/Programs/Programs';
+// import Reasons from './Components/Reasons/Reasons';
+// import Testimonials from './Components/Testimonials/Testimonials';
+
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        {/* <Hero /> */}
+        {/* <Programs /> */}
+        {/* <Reasons /> */}
+        {/* <Plan /> */}
+        {/* <Testimonials /> */}
+        {/* <Join /> */}
+        {/* <Footer /> */}
+      </div>
+    </>
+  );
+  
+}
+export default App;
