8f93ead2587d867486d78caf473aef59daca0b2c
nikita <nikitaagarwal8755@gmail.com>
2023-04-15 20:22:16 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
-// import Footer from './Components/Footer/Footer';
-// import Hero from './Components/Hero/Hero';
-// import Join from './Components/Join/Join';
-// import Plan from './Components/Plans/Plan';
-// import Programs from './Components/Programs/Programs';
-// import Reasons from './Components/Reasons/Reasons';
-// import Testimonials from './Components/Testimonials/Testimonials';
+import Footer from './Components/Footer/Footer';
+import Hero from './Components/Hero/Hero';
+import Join from './Components/Join/Join';
+import Plan from './Components/Plans/Plan';
+import Programs from './Components/Programs/Programs';
+import Reasons from './Components/Reasons/Reasons';
+import Testimonials from './Components/Testimonials/Testimonials';
 
 
 function App() {
   return (
     <>
       <div className="App">
-        {/* <Hero /> */}
-        {/* <Programs /> */}
-        {/* <Reasons /> */}
-        {/* <Plan /> */}
-        {/* <Testimonials /> */}
-        {/* <Join /> */}
-        {/* <Footer /> */}
+        <Hero />
+        <Programs />
+        <Reasons />
+        <Plan />
+        <Testimonials />
+        <Join />
+        <Footer />
       </div>
     </>
   );
